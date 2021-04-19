5fdf6a6ee04316c6f2f6c119aaf74e7eabeb1865
Halil CAN <halilc.2001@gmail.com>
2021-04-19 22:29:26 +0300

    add mentor programns

--- front-end/src/components/pages/MentorPrograms.vue
+++ front-end/src/components/pages/MentorPrograms.vue
@@ -3,17 +3,146 @@
     <page-title :text="' Mentor Programları'"/>
     <div class="content">
       <div class="mentor_programs">
+        <div class="filters">
+          <div class="competency_selection">
+            <span>
+              Mentorluk Alanı
+              <i class="bi bi-caret-down"></i>
+            </span>
+          </div>
+          <div class="program_status">
+           <span>
+              Program Durumu
+           </span>
+          </div>
+        </div>
         <ul>
           <li>
             <div class="infos">
-
+              <div class="type">
+                <i class="bi bi-alarm"></i>
+                yazılım
+              </div>
+              <div class="user_count">
+                <i class="bi bi-people"></i>
+                2 kişi
+              </div>
+              <div class="last_date">
+                <i class="bi bi-clock"></i>
+                26 ekim
+              </div>
             </div>
             <div class="inner">
               <div class="profile">
-
+                <img
+                    src="https://ui-avatars.com/api/?name=halil+can&amp;background=0288D1&amp;color=FFFFFF&amp;size=128">
+                <div class="name_surname">
+                  Halil CAN
+                </div>
+                <div class="username">
+                  @hcan
+                </div>
               </div>
               <div class="program_info">
-
+                <div class="title">
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                </div>
+                <div class="text">
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="infos">
+              <div class="type">
+                <i class="bi bi-alarm"></i>
+                yazılım
+              </div>
+              <div class="user_count">
+                <i class="bi bi-people"></i>
+                2 kişi
+              </div>
+              <div class="last_date">
+                <i class="bi bi-clock"></i>
+                sona erdi
+              </div>
+            </div>
+            <div class="inner">
+              <div class="profile">
+                <img
+                    src="https://ui-avatars.com/api/?name=halil+can&amp;background=0288D1&amp;color=FFFFFF&amp;size=128">
+                <div class="name_surname">
+                  Halil CAN
+                </div>
+                <div class="username">
+                  @hcan
+                </div>
+              </div>
+              <div class="program_info">
+                <div class="title">
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                </div>
+                <div class="text">
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="infos">
+              <div class="type">
+                <i class="bi bi-alarm"></i>
+                yazılım
+              </div>
+              <div class="user_count">
+                <i class="bi bi-people"></i>
+                2 kişi
+              </div>
+              <div class="last_date">
+                <i class="bi bi-clock"></i>
+                26 ekim
+              </div>
+            </div>
+            <div class="inner">
+              <div class="profile">
+                <img
+                    src="https://ui-avatars.com/api/?name=halil+can&amp;background=0288D1&amp;color=FFFFFF&amp;size=128">
+                <div class="name_surname">
+                  Halil CAN
+                </div>
+                <div class="username">
+                  @hcan
+                </div>
+              </div>
+              <div class="program_info">
+                <div class="title">
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                </div>
+                <div class="text">
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlıkbaşlık başlıkbaşlıkbaşlık başlık başlıkbaşlık
+                  başlıkbaşlıkbaşlık başlık başlık
+                  başlık başlıkbaşlıkbaşlık başlık başlık
+                </div>
               </div>
             </div>
           </li>
@@ -25,7 +154,40 @@
         </div>
         <ul>
           <li>
-            sdadas
+            <img src="https://ui-avatars.com/api/?name=halil+can&amp;background=0288D1&amp;color=FFFFFF&amp;size=128">
+            <div class="user_info">
+              <div class="name_surname">
+                Halil CAN
+              </div>
+              <div class="type">
+                <i class="bi bi-circle-fill"></i>
+                yazılım
+              </div>
+            </div>
+          </li>
+          <li>
+            <img src="https://ui-avatars.com/api/?name=halil+can&amp;background=0288D1&amp;color=FFFFFF&amp;size=128">
+            <div class="user_info">
+              <div class="name_surname">
+                Halil CAN
+              </div>
+              <div class="type">
+                <i class="bi bi-circle-fill"></i>
+                yazılım
+              </div>
+            </div>
+          </li>
+          <li>
+            <img src="https://ui-avatars.com/api/?name=halil+can&amp;background=0288D1&amp;color=FFFFFF&amp;size=128">
+            <div class="user_info">
+              <div class="name_surname">
+                Halil CAN
+              </div>
+              <div class="type">
+                <i class="bi bi-circle-fill"></i>
+                yazılım
+              </div>
+            </div>
           </li>
         </ul>
       </div>
@@ -52,16 +214,136 @@ export default {
 
 .content > .mentor_programs {
   width: 100%;
+  padding: 0 20px;
+}
+
+.content > .mentor_programs > .filters {
+  display: flex;
+  justify-content: flex-start;
+  padding: 10px 10px;
+  position: sticky;
+  top: 0;
+  z-index: 2;
+  background-color: #F2F2F8;
+  border-radius: 3px;
+}
+
+
+.content > .mentor_programs > .filters > div {
+  margin-right: 10px;
+  padding: 9px;
+  cursor: pointer;
+  font-family: 'Poppins', sans-serif;
+  font-size: 12px;
+  border-radius: 3px;
+}
+
+.content > .mentor_programs > .filters > div > span {
+
 }
 
 .mentor_programs > ul {
-  padding: 0;
   list-style: none;
-  background-color: red;
+  padding: 0px;
 }
 
+.mentor_programs > ul > li {
+  padding: 10px 20px;
+  cursor: pointer;
+  border: 1px solid #f3f3f3;
+  border-radius: 4px;
+  transition: .3s;
+  margin: 38px 0;
+
+  box-shadow: 0 0px 0.3px rgba(0, 0, 0, 0.011),
+  0 0px 0.7px rgba(0, 0, 0, 0.016),
+  0 0px 1.3px rgba(0, 0, 0, 0.02),
+  0 0px 2.2px rgba(0, 0, 0, 0.024),
+  0 0px 4.2px rgba(0, 0, 0, 0.029),
+  0 0px 10px rgba(0, 0, 0, 0.04);
+
+}
+
+.mentor_programs > ul > li:hover {
+  transform: translateY(-2px);
+  box-shadow: 0 0px 0.3px rgba(0, 0, 0, 0.02),
+  0 0px 0.7px rgba(0, 0, 0, 0.028),
+  0 0px 1.3px rgba(0, 0, 0, 0.035),
+  0 0px 2.2px rgba(0, 0, 0, 0.042),
+  0 0px 4.2px rgba(0, 0, 0, 0.05),
+  0 0px 10px rgba(0, 0, 0, 0.07);
+
+}
+
+.mentor_programs > ul > li > .infos {
+  display: flex;
+}
+
+.mentor_programs > ul > li > .infos > div {
+  margin-right: 17px;
+  padding: 3px 8px;
+  border-radius: 10px;
+  font-family: 'Montserrat', 'sans-serif';
+  font-size: 11px;
+  background-color: #f3f3f3;
+}
+
+.mentor_programs > ul > li > .infos > .last_date {
+  background-color: #ffeaea;
+  color: #cf2222;
+}
+
+.mentor_programs > ul > li > .inner {
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 13px;
+}
+
+.mentor_programs > ul > li > .inner > .profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.mentor_programs > ul > li > .inner > .profile > img {
+  width: 60px;
+  border-radius: var(--navy-user-profile-border-radius);
+}
+
+
+.mentor_programs > ul > li > .inner > .profile > .name_surname {
+  font-family: 'Poppins', sans-serif;
+  font-size: 12px;
+  margin-top: 5px;
+  color: var(--navy-blue-text-color);
+}
+
+.mentor_programs > ul > li > .inner > .profile > .username {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 12px;
+  color: #454545;
+  color: #2972cf;
+}
+
+.mentor_programs > ul > li > .inner > .program_info {
+  padding-left: 20px;
+}
+
+.mentor_programs > ul > li > .inner > .program_info > .title {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 15px;
+}
+
+.mentor_programs > ul > li > .inner > .program_info > .text {
+  font-family: 'Montserrat', 'sans-serif';
+  font-size: 13px;
+  margin-top: 5px;
+}
+
+
 .content > .new_mentors {
-  width: 450px;
+  width: 350px;
   margin-left: 10px;
   border-radius: 3px;
   background-color: #f2f2f8;
@@ -70,7 +352,60 @@ export default {
 
 .content > .new_mentors > .title {
   font-family: 'Poppins', sans-serif;
-  font-size: 17px;
+  font-size: 18px;
   text-align: center;
+  border-radius: 3px;
+  background-color: #e4e4ef;
+  padding: 5px 0;
+}
+
+.content > .new_mentors > ul {
+  list-style: none;
+  padding: 0 10px;
+}
+
+.content > .new_mentors > ul > li {
+  display: flex;
+  justify-content: flex-start;
+  background-color: #ededf8;
+  padding: 6px 10px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: .3s;
+  margin: 8px 0;
+}
+
+.content > .new_mentors > ul > li:hover {
+  background-color: #e8e8f3;
+}
+
+.content > .new_mentors > ul > li > img {
+  width: 50px;
+  border-radius: var(--navy-user-profile-border-radius);
+}
+
+.content > .new_mentors > ul > li > .user_info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-left: 9px;
+}
+
+.content > .new_mentors > ul > li > .user_info > .name_surname {
+  font-family: 'Poppins', sans-serif;
+  color: var(--navy-blue-text-color);
+}
+
+.content > .new_mentors > ul > li > .user_info > .type {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+}
+
+.content > .new_mentors > ul > li > .user_info > .type > i {
+  font-size: 5px;
+  margin-right: 3px;
 }
 </style>
